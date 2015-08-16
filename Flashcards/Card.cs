b27e66a230d5982c7b83d351212a0bb88c2172eb
Ryan <ryan.samarajeewa@gmail.com>
2015-08-15 21:37:58 -0700

    Resolve merge conflicts in Card object

--- Flashcards/Card.cs
+++ Flashcards/Card.cs
@@ -15,38 +15,6 @@ namespace Flashcards
 {
     class Card
     {
-<<<<<<< HEAD
-        string id = "";
-        string sideOne = "";
-        string sideTwo = "";
-        bool viewed = false;
-
-        //populates a side of a card with a string
-        public void writeToSide(int side, string content)
-        {
-            if (side == 1)
-            {
-                this.sideOne = content;
-            }
-            else
-            {
-                this.sideTwo = content;
-            }
-
-        }
-
-        //set the 'viewed' variable to true 
-        //so that it cannot be read twice in the same
-        //session.
-        public void setToViewed()
-        {
-            this.viewed = true;
-        }
-        
-||||||| merged common ancestors
-        string id = "";
-        bool viewed = false;
-=======
         public string id, side1, side2;
         public bool viewed = false;
 
@@ -58,7 +26,6 @@ namespace Flashcards
             this.side2 = side2;
             viewed = false;
         }
->>>>>>> 4ff1b8dfd3290293273a35f14510d76e887e2b19
     }
 
     
